bug fixes in ecop_nd computation

ideas/aga_ecop_nd.py
@@ -18,14 +18,14 @@ import pandas as pd
 from scipy.stats import rankdata
 import matplotlib.pyplot as plt; plt.ioff()
 
-from fcopulas import get_nd_ecop, get_srho_for_ecop_nd
+from fcopulas import (
+    get_ecop_nd, get_hist_nd, get_srho_for_ecop_nd, get_srho_plus_for_hist_nd)
 
 DEBUG_FLAG = False
 
 np.set_printoptions(
-    precision=3,
     linewidth=200000,
-    formatter={'float': '{:+0.3f}'.format})
+    formatter={'float': '{:+0.5f}'.format})
 
 
 def main():
@@ -39,6 +39,9 @@ def main():
     in_data_file = Path(
         r'neckar_full_neckar_avg_temp_kriging_1961-01-01_to_2015-12-31_1km_all__EDK.csv')
 
+    # in_data_file = Path(
+    #     r'neckar_norm_cop_infill_discharge_1961_2015_20190118.csv')
+
     in_df = pd.read_csv(in_data_file, sep=';', index_col=0)
 
 #         in_df = in_df[['2446', '420', '1462', '427']] # Plochingen.
@@ -47,23 +50,16 @@ def main():
 #         in_df = in_df[[ '1412', '477', '478', '3470']]  # Jagst.
 #         in_df = in_df[['420', '427', '3421']]
 
-#     in_df = in_df[[ '1412', '478']]
+    # in_df = in_df[[ '1412', '478']]
     in_df = in_df[[ '1412', '478', '3470']]
-#     in_df = in_df[[ '1412', '478', '3470', '427']]
-
-#     in_df = in_df[[ '1412', '1412']]  # , '1412']]
-
-#     in_df = in_df.loc['1990-01-01':'2000-12-31']
-
-#         in_df['1412'] = get_flipped_vals(in_df['1412'].values)
-#         in_df['477'] = get_flipped_vals(in_df['477'].values)
-#         in_df['478'] = get_flipped_vals(in_df['478'].values)
-#         in_df['3470'] = get_flipped_vals(in_df['3470'].values)
+    # in_df = in_df[[ '1412', '478', '3470', '427']]
 
     assert np.all(np.isfinite(in_df.values))
 
     # Station cmpr.
     in_data = in_df.values
+
+    # in_data = in_data[:30,:]
 
     probs = np.apply_along_axis(
         rankdata, 0, in_data) / (in_data.shape[0] + 1.0)
@@ -77,22 +73,55 @@ def main():
 
     n_vals = probs.shape[0]
 
-    n_bins = 30
+    n_bins = 50
 
-#     print('scorr mat:')
-#     print(np.corrcoef(probs.T))
-
-    print('Using np.arange as probs!')
-    probs = np.tile(
-        np.arange(1, n_vals + 1).reshape(-1, 1),
-        (1, n_dims)) / (n_vals + 1.0)
-
+    if False:
+        print('Using np.arange as probs!')
+        probs = np.tile(
+            np.arange(1, n_vals + 1).reshape(-1, 1),
+            (1, n_dims)) / (n_vals + 1.0)
     #==========================================================================
 
-    if True:
+    print('scorr mat:')
+    print(np.corrcoef(probs.T))
+
+    if False:
         print(n_vals, n_dims, n_bins)
         print(probs)
     #==========================================================================
+
+    if True:
+        beg_time = timeit.default_timer()
+        ecop_cyth = get_ecop_nd(probs, n_bins)
+        end_time = timeit.default_timer()
+
+        print(f'\necop_cyth took: {end_time - beg_time:0.5f} seconds.')
+        #======================================================================
+
+        beg_time = timeit.default_timer()
+        scorr_cyth = get_srho_for_ecop_nd(ecop_cyth, n_dims)
+        end_time = timeit.default_timer()
+
+        print(f'scorr_cyth_ecop took: {end_time - beg_time:0.5f} seconds.')
+
+        print('scorr ecop cyth:', scorr_cyth)
+
+    if True:
+        beg_time = timeit.default_timer()
+        hist_cyth = get_hist_nd(probs, n_bins)
+        end_time = timeit.default_timer()
+
+        print(f'\nhist_cyth took: {end_time - beg_time:0.5f} seconds.')
+        #======================================================================
+
+        beg_time = timeit.default_timer()
+        scorr_cyth_hist = get_srho_plus_for_hist_nd(
+            hist_cyth, n_dims, n_bins, n_vals)
+        end_time = timeit.default_timer()
+
+        print(f'scorr_cyth_hist took: {end_time - beg_time:0.5f} seconds.')
+
+        print('scorr cyth hist:', scorr_cyth_hist)
 
 #     # Compute relative frequencies for the empirical copula.
 #     beg_time = timeit.default_timer()
@@ -451,25 +480,6 @@ def main():
 #
 #     assert np.all(np.isclose(cudus, cudus_fast))
     #==========================================================================
-
-#     beg_time = timeit.default_timer()
-#
-    ecop_cyth = get_nd_ecop(probs, n_bins)
-#
-#     end_time = timeit.default_timer()
-#
-#     print(ecop_cyth)
-#     print(f'Ecop_cyth took: {end_time - beg_time:0.5f} seconds.')
-#
-#     assert np.all(np.isclose(ecop_cyth, cudus))
-#
-#     scorr_pyth = get_srho_for_ecop_nd(cudus, n_dims)
-#     scorr_pyth_fast = get_srho_for_ecop_nd(cudus_fast, n_dims)
-    scorr_cyth = get_srho_for_ecop_nd(ecop_cyth, n_dims)
-#
-#     print('scorr pyth:', scorr_pyth)
-#     print('scorr pyth_fast:', scorr_pyth_fast)
-    print('scorr cyth:', scorr_cyth)
 
 #     cudus_reshape = cudus.reshape(*[n_bins] * n_dims)
 #

ideas/agb_ecop_nd_cdf.py
@@ -18,6 +18,8 @@ import pandas as pd
 from scipy.stats import rankdata
 import matplotlib.pyplot as plt; plt.ioff()
 
+from fcopulas.cyth import get_ecop_nd
+
 DEBUG_FLAG = False
 
 np.set_printoptions(
@@ -108,11 +110,16 @@ def shift_by_one_ign(shift_idxs, max_idxs, ignore_idx):
 
 def main():
 
-    main_dir = Path(os.getcwd())
-    os.chdir(main_dir)
-
     main_dir = Path(r'P:\Synchronize\IWS\Testings\Copulas_practice\ecop_nd')
     os.chdir(main_dir)
+
+    # shift_idxs = np.array([0, 1, 1])
+    # max_idxs = np.array([1, 1, 1])
+    # shift_by_one_ign(shift_idxs, max_idxs, 0)
+    #
+    # print(shift_idxs)
+    #
+    # return
 
     in_data_file = Path(
         r'neckar_full_neckar_avg_temp_kriging_1961-01-01_to_2015-12-31_1km_all__EDK.csv')
@@ -125,9 +132,10 @@ def main():
 #         in_df = in_df[[ '1412', '477', '478', '3470']]  # Jagst.
 #         in_df = in_df[['420', '427', '3421']]
 
-#     in_df = in_df[[ '1412', '478']]
-    in_df = in_df[[ '1412', '478', '3470']]
-#     in_df = in_df[[ '1412', '478', '3470', '427']]
+    # in_df = in_df[[ '1412', '478']]
+    # in_df = in_df[[ '1412', '478', '3470']]
+    # in_df = in_df[[ '1412', '478', '3470', '427']]
+    # in_df = in_df[[ '1412', '478', '3470', '427', '1462']]
 
 #     in_df = in_df[[ '1412', '1412']]  # , '1412']]
 
@@ -155,151 +163,214 @@ def main():
 
     n_vals = probs.shape[0]
 
-    n_bins = 20
+    n_bins = 10
 
-#     print('scorr mat:')
-#     print(np.corrcoef(probs.T))
+    # print('scorr mat:')
+    # print(np.corrcoef(probs.T))
 
-    print('Using np.arange as probs!')
-    probs = np.tile(
-        np.arange(1, n_vals + 1).reshape(-1, 1),
-        (1, n_dims)) / (n_vals + 1.0)
+    # print('Using np.arange as probs!')
+    # probs = np.tile(
+    #     np.arange(1, n_vals + 1).reshape(-1, 1),
+    #     (1, n_dims)) / (n_vals + 1.0)
 
     #==========================================================================
 
-    if True:
+    if False:
         print(n_vals, n_dims, n_bins)
         print(probs)
     #==========================================================================
 
-    # Compute relative frequencies for the empirical copula.
+#     # Compute relative frequencies for the empirical copula.
+#     beg_time = timeit.default_timer()
+#
+#     n_cells = n_bins ** n_dims
+#     ecop = np.zeros(n_cells, dtype=np.uint64)
+#
+#     for i in range(n_vals):
+#         idxs = np.zeros(n_dims, dtype=np.uint64)
+#         for j in range(n_dims):
+#             idxs[j] = int(probs[i, j] * n_bins)
+#
+#             if j:
+#                 idxs[j] *= n_bins ** j
+#
+#         if False:
+#             print(idxs, idxs.sum())
+#
+#         ecop[idxs.sum(dtype=np.uint64)] += 1
+#
+#     end_time = timeit.default_timer()
+#
+#     print(f'Histogram took: {end_time - beg_time:0.6f} seconds.')
+#
+# #     hist_time_pyth = end_time - beg_time
+#
+# #     ecop /= n_vals
+#
+#     ecop_reshape = ecop.reshape(*[n_bins] * n_dims)
+#
+#     if False:
+#         print('ecop')
+#         print(ecop_reshape)
+#         print(ecop.sum())
+#     #==========================================================================
+#
+#     print('\n\nCumm')
+#     # Cummulative sum of ecop array.
+#     sclr_arr = np.array(
+#         [n_bins ** i for i in range(0, n_dims)], dtype=np.int64)
+#
+#     cudus = np.zeros_like(ecop)
+#
+#     zeros = np.zeros(n_dims, dtype=np.int64)
+#     beg_time = timeit.default_timer()
+#
+#     rpt_ctr = 0
+#     tot_ctr = 0
+#     excpt_ctr = 0
+#     for i in range(n_cells):
+#         idxs = np.zeros(n_dims, dtype=np.int64)
+#
+#         # Get indices back for a given index in ecop.
+#         for j in range(n_dims):
+#             idxs[j] = int(i // (n_bins ** j)) % n_bins
+#
+#         if False:
+#             print(i, idxs)  # * sclr_arr
+#
+#         if False:
+#             cudu = 0.0
+#             cidxs = np.zeros(n_dims, dtype=np.int64)
+#
+#             ctr = 0
+#             while ctr <= i:
+#                 # This can be further optimized.
+#                 sel_flag = True
+#                 for j in range(n_dims):
+#                     cidxs[j] = int(ctr / (n_bins ** j)) % n_bins
+#
+#                     # Small opt. went from 7.6 to 5.6 secs.
+#                     if cidxs[j] > idxs[j]:
+#                         sel_flag = False
+#                         break
+#
+#                 if sel_flag:
+#                     cudu += ecop[(cidxs * sclr_arr).sum()]
+#
+#                 ctr += 1
+#
+#             cudus[i] = cudu
+#         #======================================================================
+#
+#         # Right now, the idea is to take the cdf value from cell
+#         # that is directly behind and take the ecop hist values
+#         # for the remaining ones. Cells that have been visited must
+#         # be accounted for and no cell should be visited twice.
+#         # Corner cases are taken care of.
+#         else:
+#             # Was four times faster when using 20 bins in 3D.
+#             # Should become faster and faster as the number of bins
+#             # increase, compared to the primitive version.
+#
+#             hind_idxs = np.maximum(zeros, idxs - 1)
+#             cudu = cudus[(hind_idxs * sclr_arr).sum(dtype=np.uint64)]
+#
+#             # if i == (np.array([1, 1, 1, 0]) * sclr_arr).sum():
+#             #     _ = 1
+#
+#                 # print(ecop_reshape)
+#                 # print(cudus.reshape(*[n_bins] * n_dims))
+#
+#             visited_cells = np.zeros(i, dtype=int)
+#
+#             # Keep track that one thing is not visited more than once per
+#             # iteration.
+#             for k in range(n_dims):
+#                 if not idxs[k]:
+#                     continue
+#
+#                 max_idxs = idxs.copy()
+#
+#                 if (k + 1) == n_dims:
+#                     max_idxs[0] = max(idxs[0] - 1, 0)
+#                     # max_idxs[0] = idxs[0] - 1
+#
+#                 else:
+#                     max_idxs[k + 1] = max(0, idxs[k + 1] - 1)
+#                     # max_idxs[k + 1] = idxs[k + 1] - 1
+#
+#                 if np.any(max_idxs < 0):
+#                     continue
+#
+#                 shift_idxs = np.zeros(n_dims, dtype=np.int64)
+#
+#                 shift_idxs[k] = idxs[k]
+#
+#                 # assert not np.all(shift_idxs == hind_idxs), (i, k, shift_idxs, hind_idxs)
+#                 # assert not np.all(shift_idxs == idxs), (i, k, shift_idxs, idxs)
+#
+#                 while (max_idxs - shift_idxs).sum() > 0:
+#
+#                     ecop_idx = (shift_idxs * sclr_arr).sum(dtype=np.uint64)
+#
+#                     if not visited_cells[ecop_idx]:
+#                         cudu += ecop[ecop_idx]
+#                         visited_cells[ecop_idx] += 1
+#
+#                     else:
+#                         rpt_ctr += 1
+#
+#                     tot_ctr += 1
+#
+#                     if shift_by_one_ign(shift_idxs, max_idxs, k):
+#                         pass
+#
+#                     else:
+#                         break
+#
+#                 ecop_idx = (shift_idxs * sclr_arr).sum(dtype=np.uint64)
+#
+#                 if ecop_idx != i:
+#                     if not visited_cells[ecop_idx]:
+#                         cudu += ecop[ecop_idx]
+#                         visited_cells[ecop_idx] += 1
+#
+#                     else:
+#                         rpt_ctr += 1
+#
+#                     tot_ctr += 1
+#
+#                 else:
+#                     excpt_ctr += 1
+#
+#             cudus[i] = cudu + ecop[(idxs * sclr_arr).sum(dtype=np.uint64)]
+#
+#     end_time = timeit.default_timer()
+#
+#     cudus_reshape = cudus.reshape(*[n_bins] * n_dims)
+#
+#     print(tot_ctr, rpt_ctr, excpt_ctr)
+#
+#     # print(cudus_reshape)
+#
+#     print(f'Cumm took: {end_time - beg_time:0.6f} seconds.')
+    #==========================================================================
+
     beg_time = timeit.default_timer()
 
-    n_cells = n_bins ** n_dims
-    ecop = np.zeros(n_cells)
-
-    for i in range(n_vals):
-        idxs = np.zeros(n_dims, dtype=np.uint64)
-        for j in range(n_dims):
-            idxs[j] = int(probs[i, j] * n_bins)
-
-            if j:
-                idxs[j] *= n_bins ** j
-
-        if False:
-            print(idxs, idxs.sum())
-
-        ecop[idxs.sum(dtype=np.uint64)] += 1
+    cudus_cyth = get_ecop_nd(probs, n_bins)
 
     end_time = timeit.default_timer()
 
-    print(f'Histogram took: {end_time - beg_time:0.5f} seconds.')
+    # cudus_cyth_reshape = cudus_cyth.reshape(*[n_bins] * n_dims)
 
-#     hist_time_pyth = end_time - beg_time
+    # print(cudus_cyth_reshape)
 
-#     ecop /= n_vals
+    print(f'Cython took: {end_time - beg_time:0.6f} seconds.')
 
-    ecop_reshape = ecop.reshape(*[n_bins] * n_dims)
+    # print(cudus_reshape == cudus_cyth_reshape)
 
-    if True:
-        print('ecop')
-        print(ecop_reshape)
-        print(ecop.sum())
-
-    #==========================================================================
-
-    print('\n\nCumm')
-    # Cummulative sum of ecop array.
-    sclr_arr = np.array(
-        [n_bins ** i for i in range(0, n_dims)], dtype=np.int64)
-
-    cudus = np.zeros_like(ecop)
-
-    zeros = np.zeros(n_dims, dtype=np.int64)
-    beg_time = timeit.default_timer()
-    for i in range(n_cells):
-        idxs = np.zeros(n_dims, dtype=np.int64)
-
-        # Get indices back for a given index in ecop.
-        for j in range(n_dims):
-            idxs[j] = int(i / (n_bins ** j)) % n_bins
-
-        if False:
-            print(i, idxs)  # * sclr_arr
-
-        if True:
-            cudu = 0.0
-            cidxs = np.zeros(n_dims, dtype=np.int64)
-
-            ctr = 0
-            while ctr <= i:
-                # This can be further optimized.
-                sel_flag = True
-                for j in range(n_dims):
-                    cidxs[j] = int(ctr / (n_bins ** j)) % n_bins
-
-                    # Small opt. went from 7.6 to 5.6 secs.
-                    if cidxs[j] > idxs[j]:
-                        sel_flag = False
-                        break
-
-                if sel_flag:
-                    cudu += ecop[(cidxs * sclr_arr).sum()]
-
-                ctr += 1
-
-            cudus[i] = cudu
-        #======================================================================
-
-        # Right now, the idea is to take the cdf value from cell
-        # that is directly behind and take the ecop hist values
-        # for the remaining ones. Cells that have been visited must
-        # be accounted for and not cell should be visited twice.
-        # Corner cases are taken care of.
-        else:
-            # Was four times faster when using 20 bins in 3D.
-            # Should become faster and faster as the number of bins
-            # increase, compared to the primitive version.
-            bidxs = np.maximum(zeros, idxs.astype(np.int64) - 1)
-
-            cudu = cudus[(bidxs * sclr_arr).sum(dtype=np.uint64)]
-
-            for k in range(n_dims):
-                shift_idxs = np.zeros(n_dims, dtype=np.int64)
-
-                max_idxs = idxs.copy()
-
-                shift_idxs[k] = idxs[k]
-
-                # Limit one side of the idxs so no cell is visited more than once.
-                if (k + 1) == n_dims:
-                    max_idxs[0] = max(0, idxs[0] - 1)
-
-                else:
-                    max_idxs[k + 1] = max(0, idxs[k + 1] - 1)
-
-                while (max_idxs - shift_idxs).sum() > 0:
-                    if shift_by_one_ign(shift_idxs, max_idxs, k):
-
-                        ecop_idx = (shift_idxs * sclr_arr).sum(dtype=np.uint64)
-
-                        cudu += ecop[ecop_idx]
-
-                    else:
-                        break
-
-            cudus[i] = cudu + ecop[(idxs * sclr_arr).sum(dtype=np.uint64)]
-
-    end_time = timeit.default_timer()
-
-#     print(cudus)
-
-    cudus_reshape = cudus.reshape(*[n_bins] * n_dims)
-
-    print(cudus_reshape)
-
-    print(f'Cumm took: {end_time - beg_time:0.2f} seconds.')
-    #==========================================================================
+    # assert (cudus_reshape == cudus_cyth_reshape).all()
 
     return
 

ideas/aj_ecop_nd__with_pdf.py
@@ -19,7 +19,7 @@ from scipy.stats import rankdata
 import matplotlib.pyplot as plt; plt.ioff()
 
 from fcopulas import (
-    get_nd_ecop,
+    get_ecop_nd,
     get_srho_for_ecop_nd,
     get_srho_plus_for_hist_nd,
     get_hist_nd)
@@ -188,7 +188,7 @@ def main():
 
     if False:
         beg_time = timeit.default_timer()
-        ecop_cyth = get_nd_ecop(probs, n_bins)
+        ecop_cyth = get_ecop_nd(probs, n_bins)
         scorr_cyth = get_srho_for_ecop_nd(ecop_cyth, n_dims)
         end_time = timeit.default_timer()
 

ideas/ana_sample_from_sid_tcop.py
@@ -219,8 +219,8 @@ def get_sid_tcop_probs_auto(args):
 
     Ws = np.empty((N, 2))
 
-    Ws[:, 0] = rv1.ppf(1 - Ss)
-    Ws[:, 1] = rv2.ppf(1 - Ss)
+    Ws[:, 0] = rv1.ppf(Ss)
+    Ws[:, 1] = rv2.ppf(Ss)
     for i in range(1, N):
         Ws[i,:] += Ws[i - 1,:] * pcorr
 
@@ -265,7 +265,7 @@ def main():
 
     # Skewness constants.
     # Apparently, for order asymm.
-    Gs = np.array([-1.0, -1.0, ])
+    Gs = np.array([-0.0, -0.0, ])
 
     # Copula type
     # cop_type = 'cross'
@@ -275,13 +275,15 @@ def main():
     if cop_type == 'cross':
         probs = get_sid_tcop_probs_cross((pcorr, mean, cov, nu1, nu2, N, *Gs))
 
+        plt.scatter(probs[:, 0], probs[:, 1], alpha=10 / 255.0)
+
     elif cop_type == 'auto':
         probs = get_sid_tcop_probs_auto((pcorr, mean, cov, nu1, nu2, N, *Gs))
 
+        plt.scatter(probs[:-1], probs[1:], alpha=10 / 255.0)
+
     else:
         raise ValueError(f'Unknown cop_type: {cop_type}!')
-
-    plt.scatter(probs[:, 0], probs[:, 1], alpha=10 / 255.0)
 
     plt.title(
         f'Copula type: {cop_type}\n'

ideas/anb_fit_sid_tcop.py
@@ -88,17 +88,17 @@ def main():
     read_col = '454'
 
     # Copula stuff.
-    n_bins = 20
+    n_bins = 10
 
-    max_lag = 20
+    max_lag = 10
 
-    wts_prms = np.array([1.0, 5.0, 5.0, 0.0])
+    wts_prms = np.array([1.0, 1.0, 0.0, 0.0])
 
-    shift_prms = np.array([+0.001, -0.001, -0.001, -0.001])
+    shift_prms = np.array([+0.000, -0.200, -0.000, -0.000])
 
     # Optimization parameters.
     n_cpus = 8
-    max_iters = 100
+    max_iters = 1000
     opt_tol = 0.01
 
     # Copula type to evaluate.
@@ -248,8 +248,8 @@ def main():
     # Temporal props.
     axes = plt.subplots(2, 3, squeeze=False, figsize=None)[1]
 
-    plot_props(in_probs.reshape(-1, 1), 20, n_bins, axes, 'r', 'ref')
-    plot_props(sim_probs.reshape(-1, 1), 20, n_bins, axes, 'b', 'sim')
+    plot_props(in_probs.reshape(-1, 1), max_lag, n_bins, axes, 'r', 'ref')
+    plot_props(sim_probs.reshape(-1, 1), max_lag, n_bins, axes, 'b', 'sim')
 
     # For now.
     axes[0, 2].set_axis_off()